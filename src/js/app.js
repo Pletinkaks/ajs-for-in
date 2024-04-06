24533ab7b07f58d68a85f920b8ff1808e1640525
Ksenia Pletinka <pletinkaksenia@gmail.com>
2024-04-06 17:33:36 +0300

    second commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,5 @@
-export default function sortProp(obj, order) {
-  const objList = [];
-  const specObjList = [];
+import orderByProps from './orderbyprops';
 
-  order.forEach((key) => specObjList.push({ key, value: obj[key] }));
-
-  for (const key in obj) {
-    if (Object.prototype.hasOwnProperty.call(obj, key)) {
-      if (!order.includes(key)) objList.push({ key, value: obj[key] });
-    }
-  }
-
-  objList.sort((a, b) => (a.key < b.key && -1) || (a.key > b.key && 1) || 0);
-
-  return [...specObjList, ...objList];
-}
+const obj = { name: "мечник", health: 10, level: 2, attack: 80, defence: 40 };
+const res = orderByProps(obj, ["name", "level"]);
+console.log(res);
